4c4fe8877621cceb7bd981359b4628276383311e
Yū Kobayashi <kb10uy@kb10uy.org>
2023-02-18 08:53:21 +0900

    set parameters

--- src/decl/document.rs
+++ src/decl/document.rs
@@ -1,21 +1,21 @@
-/*
 use crate::decl::{
-    animations::{Animations, NODE_NAME_ANIMATIONS},
-    drivers::{Drivers, NODE_NAME_DRIVERS},
-    get_argument,
-    menu::{Menu, NODE_NAME_MENU},
+    deconstruct_node,
+    // animations::{Animations, NODE_NAME_ANIMATIONS},
+    // drivers::{Drivers, NODE_NAME_DRIVERS},
+    // menu::{Menu, NODE_NAME_MENU},
     parameters::{Parameters, NODE_NAME_PARAMETERS},
-    split_entries, DeclError, Result,
+    DeclError,
+    DeclErrorKind,
+    Result,
 };
-*/
-use std::collections::HashMap;
 
-use kdl::{KdlDocument, KdlNode, KdlValue};
+use kdl::{KdlDocument, KdlNode};
+use miette::{SourceOffset, SourceSpan};
 use semver::Version;
 
 pub const NODE_NAME_VERSION: &str = "version";
 pub const NODE_NAME_AVATAR: &str = "avatar";
-/*
+
 #[derive(Debug, Clone)]
 pub struct Document {
     version: Version,
@@ -23,15 +23,27 @@ pub struct Document {
 }
 
 impl Document {
-    pub fn parse(document: &KdlDocument) -> Result<Document> {
-        // Detect version
+    pub fn parse(document: &KdlDocument, source: &str) -> Result<Document> {
         let nodes = document.nodes();
+        let first_span = SourceSpan::new(
+            SourceOffset::from_location(source, 1, 1),
+            SourceOffset::from_location(source, 1, 1),
+        );
+
+        // Detect version
         let Some(version_node) = nodes.get(0) else {
-            return Err(DeclError::NodeNotFound(NODE_NAME_VERSION, "root document"))
+            return Err(DeclError::new(source, &first_span, DeclErrorKind::NodeNotFound(NODE_NAME_VERSION)));
         };
-        let (version_args, _) = split_entries(version_node.entries());
-        let version_text = get_argument(&version_args, 0, "version")?;
-        let version = Version::parse(version_text)?;
+        let (_, entries, _) =
+            deconstruct_node(source, version_node, Some(NODE_NAME_VERSION), Some(false))?;
+        let version_text = entries.get_argument(0, "version")?;
+        let version = Version::parse(version_text).map_err(|e| {
+            DeclError::new(
+                source,
+                version_node.name().span(),
+                DeclErrorKind::VersionError(e),
+            )
+        })?;
 
         // Other nodes
         let mut avatar = None;
@@ -40,16 +52,28 @@ impl Document {
             match node_name {
                 NODE_NAME_AVATAR => match avatar {
                     None => {
-                        avatar = Some(node.parse(&version)?);
+                        avatar = Some(Avatar::parse(node, source)?);
                     }
-                    _ => return Err(DeclError::DuplicateNodeFound(NODE_NAME_VERSION)),
+                    _ => {
+                        return Err(DeclError::new(
+                            source,
+                            node.name().span(),
+                            DeclErrorKind::DuplicateNodeFound,
+                        ))
+                    }
                 },
-                otherwise => return Err(DeclError::InvalidNodeDetected(otherwise.to_string())),
+                otherwise => {
+                    return Err(DeclError::new(
+                        source,
+                        node.name().span(),
+                        DeclErrorKind::InvalidNodeDetected,
+                    ))
+                }
             }
         }
 
         let Some(avatar) = avatar else {
-            return Err(DeclError::NodeNotFound(NODE_NAME_AVATAR, "root document"))
+            return Err(DeclError::new(source, &first_span, DeclErrorKind::NodeNotFound(NODE_NAME_VERSION)));
         };
         Ok(Document { version, avatar })
     }
@@ -59,49 +83,46 @@ impl Document {
 #[derive(Debug, Clone)]
 pub struct Avatar {
     name: String,
-    animations_blocks: Vec<Animations>,
-    drivers_blocks: Vec<Drivers>,
+    // animations_blocks: Vec<Animations>,
+    // drivers_blocks: Vec<Drivers>,
     parameters_blocks: Vec<Parameters>,
-    menu_blocks: Vec<Menu>,
+    // menu_blocks: Vec<Menu>,
 }
 
-impl DeclNode for Avatar {
-    const NODE_NAME: &'static str = NODE_NAME_AVATAR;
+impl Avatar {
+    pub fn parse(node: &KdlNode, source: &str) -> Result<Self> {
+        let (_, entries, children) =
+            deconstruct_node(source, node, Some(NODE_NAME_AVATAR), Some(true))?;
 
-    const CHILDREN_EXISTENCE: Option<bool> = Some(true);
-
-    fn parse(
-        version: &Version,
-        _name: &str,
-        args: &[&KdlValue],
-        _props: &HashMap<&str, &KdlValue>,
-        children: &[KdlNode],
-    ) -> Result<Self> {
-        let avatar_name = get_argument(&args, 0, "name")?;
-
-        let mut animations_blocks = vec![];
-        let mut drivers_blocks = vec![];
+        let name = entries.get_argument(0, "name")?;
+        // let mut animations_blocks = vec![];
+        // let mut drivers_blocks = vec![];
         let mut parameters_blocks = vec![];
-        let mut menu_blocks = vec![];
+        // let mut menu_blocks = vec![];
 
         for child in children {
             let child_name = child.name().value();
             match child_name {
-                NODE_NAME_ANIMATIONS => animations_blocks.push(child.parse(version)?),
-                NODE_NAME_DRIVERS => drivers_blocks.push(child.parse(version)?),
-                NODE_NAME_PARAMETERS => parameters_blocks.push(child.parse(version)?),
-                NODE_NAME_MENU => menu_blocks.push(child.parse(version)?),
-                otherwise => return Err(DeclError::InvalidNodeDetected(otherwise.into())),
+                // NODE_NAME_ANIMATIONS => animations_blocks.push(child.parse(version)?),
+                // NODE_NAME_DRIVERS => drivers_blocks.push(child.parse(version)?),
+                NODE_NAME_PARAMETERS => parameters_blocks.push(Parameters::parse(child, source)?),
+                // NODE_NAME_MENU => menu_blocks.push(child.parse(version)?),
+                otherwise => {
+                    return Err(DeclError::new(
+                        source,
+                        child.name().span(),
+                        DeclErrorKind::InvalidNodeDetected,
+                    ))
+                }
             }
         }
 
         Ok(Avatar {
-            name: avatar_name,
-            animations_blocks,
-            drivers_blocks,
+            name,
+            // animations_blocks,
+            // drivers_blocks,
             parameters_blocks,
-            menu_blocks,
+            // menu_blocks,
         })
     }
 }
-*/
