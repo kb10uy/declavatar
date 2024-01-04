74b889af278027de3b718ed5a18b4d1001eedbf7
Yū Kobayashi <kb10uy@kb10uy.org>
2024-01-05 04:12:51 +0900

    Add indeteminate error

--- declavatar/src/avatar_v2/log.rs
+++ declavatar/src/avatar_v2/log.rs
@@ -40,6 +40,8 @@ pub enum Log {
     LayerMustBeRaw(String),
     #[log_error("layer.group.invalid_copy")]
     LayerGroupInvalidCopy,
+    #[log_error("layer.switch.indeterminate")]
+    LayerSwitchIndeterminateSource,
     #[log_error("layer.puppet.cant_drive")]
     LayerPuppetCannotDrive,
     #[log_error("layer.puppet.must_inlined")]
