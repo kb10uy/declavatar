62225959afa265af21a84b9445141c0837a05e8e
Yū Kobayashi <kb10uy@kb10uy.org>
2024-02-04 17:01:18 +0900

    fix test

--- declavatar/tests/sexpr_example.rs
+++ declavatar/tests/sexpr_example.rs
@@ -1,34 +1,27 @@
 use std::{fs::read_to_string, path::PathBuf};
 
 use declavatar::{
-    avatar_v2::transform_avatar,
-    decl_v2::{compile_declaration, DeclarationFormat, PreprocessData},
+    avatar_v2::Transformer,
+    decl_v2::{compile_declaration, Arguments, DeclarationFormat},
 };
 use once_cell::sync::Lazy;
 use pretty_assertions::assert_eq;
 use rstest::*;
 
-static SEXPR_FORMAT: Lazy<DeclarationFormat> = Lazy::new(|| {
+static TEST_ARGUMENTS: Lazy<Arguments> = Lazy::new(|| {
     let extension_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
         .join("../declavatar-extra/sexpr")
         .canonicalize()
         .expect("should be valid path");
-    DeclarationFormat::Sexpr(vec![extension_dir])
-});
 
-static TEST_PREPROCESS_DATA: Lazy<PreprocessData> = Lazy::new(|| {
-    let symbols = vec!["declavatar-test", "out-of-unity"];
-    let localizations = vec![
-        ("cargo-pkg-version", env!("CARGO_PKG_VERSION")),
-        ("cargo-pkg-authors", env!("CARGO_PKG_AUTHORS")),
-    ];
-    PreprocessData {
-        symbols: symbols.into_iter().map(|s| s.to_string()).collect(),
-        localizations: localizations
-            .into_iter()
-            .map(|(k, v)| (k.to_string(), v.to_string()))
-            .collect(),
-    }
+    let mut args = Arguments::new();
+    args.add_library_path(extension_dir);
+    args.define_symbol("declavatar-test");
+    args.define_symbol("out-of-unity");
+    args.define_localization("cargo-pkg-version", env!("CARGO_PKG_VERSION"));
+    args.define_localization("cargo-pkg-authors", env!("CARGO_PKG_AUTHORS"));
+
+    args
 });
 
 #[rstest]
@@ -36,10 +29,13 @@ fn compiles_all_sexpr_examples(#[files("../examples/sexpr/*.declisp")] filename:
     let source = read_to_string(&filename).expect("source file should exist");
 
     println!("compiling {:?}", filename.canonicalize().unwrap());
-    let decl_avatar = compile_declaration(&source, SEXPR_FORMAT.clone(), TEST_PREPROCESS_DATA.clone())
-        .expect("declaration file load failure");
+    let decl_avatar =
+        compile_declaration(&source, DeclarationFormat::Sexpr, TEST_ARGUMENTS.clone())
+            .expect("declaration file load failure");
 
-    let avatar = transform_avatar(decl_avatar);
+    let transformer = Transformer::new();
+    let avatar = transformer.transform_avatar(decl_avatar);
+
     assert!(avatar.avatar.is_some());
     assert_eq!(avatar.logs, vec![]);
 }
